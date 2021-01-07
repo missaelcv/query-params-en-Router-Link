94da2fd246efa89f89d217cf591447f8548ba4a7
MisaWeb <elbilpapi@gmail.com>
2021-01-06 23:32:05 -0400

    query de router-link

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <router-link :to="{query: {seccion: 1 }}" 
+    class="btn btn-primary btn-block">1</router-link>
+
+    <br><br>
+    <router-link :to="{query: {seccion: 2 }}" 
+    class="btn btn-primary btn-block">2</router-link>
+
+    <br><br>
+    <router-link :to="{query: {seccion: 3 }}" 
+    class="btn btn-primary btn-block">3</router-link>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  watch : {
+    "$route.query.seccion":{
+      immediate: true,
+      handler(seccion){
+        console.log(`Seccion a cambiado: ${seccion}`);
+      }
+    }
+  },
+}
+</script>
